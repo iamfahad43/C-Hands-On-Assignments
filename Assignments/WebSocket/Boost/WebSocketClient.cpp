481249ec07db51932f627ccef5d991b1a26867f7
=Fahad <itsFahad.pro43@gmail.com>
2021-10-26 05:35:17 +0530

    Done collecting data from 'test.deribit.com' (collection of trades and book update against that trades)

--- Assignments/WebSocket/Boost/WebSocketClient.cpp
+++ Assignments/WebSocket/Boost/WebSocketClient.cpp
@@ -1,11 +1,19 @@
 #include <boost/beast/core.hpp>
-#include <boost/beast.hpp>
 #include <boost/beast/websocket.hpp>
 #include <boost/asio/connect.hpp>
 #include <boost/asio/ip/tcp.hpp>
+#include <boost/beast/ssl.hpp>
+#include <boost/beast/websocket/ssl.hpp>
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <openssl/sha.h>
+#include <openssl/hmac.h>
+#include <openssl/evp.h>
+#include <openssl/buffer.h>
+#include <openssl/bio.h>
+
+namespace ssl = boost::asio::ssl;       // from <boost/asio/ssl.hpp>
 
 namespace beast = boost::beast;         // from <boost/beast.hpp>
 namespace http = beast::http;           // from <boost/beast/http.hpp>
@@ -19,6 +27,7 @@ int main(int argc, char** argv)
     try
     {
         // Check command line arguments.
+/***
         if(argc != 4)
         {
             std::cerr <<
@@ -27,49 +36,79 @@ int main(int argc, char** argv)
                 "    websocket-client-sync echo.websocket.org 80 \"Hello, world!\"\n";
             return EXIT_FAILURE;
         }
-        auto const host = argv[1];
-        auto const port = argv[2];
-        auto const text = argv[3];
-
+**/
+        auto const host = "test.deribit.com";
+        auto const port = "443";
+        auto const text = R"(
+{"method": "public/subscribe", "params": {"channels": ["trades.future.BTC.raw", "book.BTC-PERPETUAL.raw"]},"jsonrpc": "2.0","id": 4}
+)";
+/******
+        auto const text = R"(
+{
+  "method": "public/get_instruments",
+  "params": {
+    "currency": "BTC",
+    "kind": "future"
+  },
+  "jsonrpc": "2.0",
+  "id": 0
+}
+)";
+*******/
         // The io_context is required for all I/O
         net::io_context ioc;
 
         // These objects perform our I/O
+//        tcp::resolver resolver{ioc};
+//        websocket::stream<tcp::socket> ws{ioc};
+
+        ssl::context ctx{ssl::context::sslv23_client};
+
+        // These objects perform our I/O
         tcp::resolver resolver{ioc};
-        websocket::stream<tcp::socket> ws{ioc};
+        websocket::stream<beast::ssl_stream<tcp::socket>> ws{ioc, ctx};
 
         // Look up the domain name
         auto const results = resolver.resolve(host, port);
 
         // Make the connection on the IP address we get from a lookup
-        net::connect(ws.next_layer(), results.begin(), results.end());
-	
-	// Set a decorator to change the User-Agent of the handshake
-	ws.set_option(websocket::stream_base::decorator(
+//        net::connect(ws.next_layer(), results.begin(), results.end());
+        net::connect(ws.next_layer().next_layer(), results.begin(), results.end());
+
+	ws.next_layer().handshake(ssl::stream_base::client);
+
+        // Set a decorator to change the User-Agent of the handshake
+        ws.set_option(websocket::stream_base::decorator(
             [](websocket::request_type& req)
             {
                 req.set(http::field::user_agent,
                     std::string(BOOST_BEAST_VERSION_STRING) +
                         " websocket-client-coro");
             }));
+
+
         // Perform the websocket handshake
-        ws.handshake(host, "/");
+        ws.handshake(host, "/ws/api/v2");
         // Send the message
         ws.write(net::buffer(std::string(text)));
-
         // This buffer will hold the incoming message
-        beast::flat_buffer buffer;
+        //beast::flat_buffer buffer;
 
-        // Read a message into our buffer
-        ws.read(buffer);
+	while(true) {
 
+        	beast::flat_buffer buffer;
+
+        	// Read a message into our buffer
+        	ws.read(buffer);
+
+        	std::cout << beast::make_printable(buffer.data()) << std::endl;
+		
+	}
         // Close the WebSocket connection
         ws.close(websocket::close_code::normal);
-
         // If we get here then the connection is closed gracefully
 
         // The make_printable() function helps print a ConstBufferSequence
-        std::cout << beast::make_printable(buffer.data()) << std::endl;
     }
     catch(std::exception const& e)
     {
@@ -78,3 +117,6 @@ int main(int argc, char** argv)
     }
     return EXIT_SUCCESS;
 }
+           
+
+
